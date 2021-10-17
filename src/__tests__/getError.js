849fd38365bf7635f1b98f0e7e5a26d66d9e5ec1
freeton20 <doctor4891@gmail.com>
2021-10-17 09:48:38 +0300

    format errors

--- src/__tests__/getError.js
+++ src/__tests__/getError.js
@@ -1,9 +1,12 @@
 const { getAst } = require("../index");
+const { formatErrors } = require("../formatErrors");
 const path = require("path");
 jest.setTimeout(100_000);
 
 test("Get AST data", async () => {
     const solFile = path.resolve(__dirname, "contracts/HelloWalletWithError.sol");
     const received_error = (await getAst(solFile));
+    expect(received_error.type).toStrictEqual("error");
+    const formattedError = formatErrors(received_error.error[0]);
     expect(received_error.type).toStrictEqual("error");
 });
---
+++ src/formatErrors.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+
+function getErrorLenght(errorString) {
+    if (!errorString) return null;
+
+    let errorStringCounter = errorString.match(/[\^]/g);
+    if (errorStringCounter == null) {
+        return null;
+    }
+    return errorStringCounter.length;
+}
+
+function getErrorFilePath(string) {
+    let filePath = string.match(/\s([\w\.\/]+\.sol):/);
+    if (filePath == null || !filePath[1]) return null;
+
+    if (fs.existsSync(filePath[1])) {
+        return filePath[1];
+    }
+   return null;
+}
+
+function formatErrors(string) {
+    if (!string) return;
+
+    let arr = string.split("\n\n");
+
+    let a = arr.map(value => {
+        return value.split("\n")
+    })
+
+    a = a.filter(value => {
+        if (value.length < 5) return false;
+        return true;
+    })
+    return a.map((value) => {
+        let coord = value[1] ? value[1].match(/\d+:\d+/) : null;
+        if (coord !== null) coord = coord[0].split(":");
+        let severity = value[0].match(/Warning/) ? 'Warning' : 'Error';
+        let raw = !coord ? null : Number(coord[0]);
+        let position = !coord ? null : Number(coord[1]);
+        let errorLenght = getErrorLenght(value[4]);
+        let source = getErrorFilePath(value.join("\n"));
+        return {
+            info: value.join('\n\n'),
+            coord: {
+                raw,
+                position
+            },
+            errorLenght,
+            severity,
+            source
+        }
+    })
+}
+module.exports = {
+    formatErrors
+}
